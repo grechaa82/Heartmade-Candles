d1938cb30f2086294733f997bbb1082c89f629cd
Grechaa8.2 <danilgusejnov@gmail.com>
2023-03-15 22:11:28 +0300

    Added Status for Order and method ChangeStatus

--- src/HeartmadeCandles/HeartmadeCandles.Core/Models/Order.cs
+++ src/HeartmadeCandles/HeartmadeCandles.Core/Models/Order.cs
@@ -3,21 +3,44 @@
     public class Order : ModelBase
     {
         public Order(
-    User user,
-    Customer customer,
-    List<Candle> candles,
-    string description)
+            User user,
+            Customer customer,
+            List<Candle> candles,
+            string description,
+            Status status = Status.New)
         {
             User = user;
             Customer = customer;
             Candles = candles;
+            Status = status;
             Description = description;
             CreatedAt = DateTime.UtcNow;
         }
+
         public User User { get; }
         public Customer Customer { get; }
         public List<Candle> Candles { get; }
+        public Status Status { get; }
         public string Description { get; }
         public DateTime CreatedAt { get; }
+
+        public Order ChangeStatus(Order order, Status newStatus)
+        {
+            return new Order(
+                order.User,
+                order.Customer,
+                order.Candles,
+                order.Description,
+                newStatus);
+        }
+    }
+    public enum Status
+    {
+        New,
+        Paid,
+        InProgress,
+        Completed,
+        Shipped,
+        Done
     }
 }
