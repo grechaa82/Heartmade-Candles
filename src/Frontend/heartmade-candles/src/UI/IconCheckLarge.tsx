2a2c6fb466ef56f100719832f734784b73bd8b26
Grechaa8.2 <danilgusejnov@gmail.com>
2023-05-26 11:12:51 +0300

    Fixed svg

--- src/Frontend/heartmade-candles/src/UI/IconCheckLarge.tsx
+++ src/Frontend/heartmade-candles/src/UI/IconCheckLarge.tsx
@@ -5,8 +5,8 @@ const IconCheckLarge: React.FC<IconProps> = ({ color = '#000' }) => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M19.7304 6.31701C20.1076 6.72041 20.0864 7.35322 19.683 7.73043L10.058 16.7304C9.67363 17.0898 9.07649 17.0899 8.69209 16.7305L4.31709 12.6405C3.91365 12.2633 3.89234 11.6305 4.2695 11.2271C4.64667 10.8236 5.27947 10.8023 5.68292 11.1795L9.37493 14.631L18.317 6.26958C18.7204 5.89237 19.3532 5.91361 19.7304 6.31701Z"
         fill={color}
       />
--- src/Frontend/heartmade-candles/src/UI/IconChevronUpLarge.tsx
+++ src/Frontend/heartmade-candles/src/UI/IconChevronUpLarge.tsx
@@ -5,8 +5,8 @@ const IconChevronUpLarge: React.FC<IconProps> = ({ color = '#000' }) => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M6.70711 15.7039C6.31658 16.0987 5.68342 16.0987 5.29289 15.7039C4.90237 15.3092 4.90237 14.6691 5.29289 14.2744L11.2929 8.29607C11.6834 7.90131 12.3166 7.90131 12.7071 8.29607L18.7071 14.2744C19.0976 14.6691 19.0976 15.3092 18.7071 15.7039C18.3166 16.0987 17.6834 16.0987 17.2929 15.7039L12 10.4404L6.70711 15.7039Z"
         fill={color}
       />
