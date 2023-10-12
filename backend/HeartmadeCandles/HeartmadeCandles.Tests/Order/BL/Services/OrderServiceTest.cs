26f6106941321546daa06223623d8a9350f76325
Grechaa8.2 <danilgusejnov@gmail.com>
2023-10-12 14:59:15 +0300

    test: added tests for OrderService.Get

--- backend/HeartmadeCandles/HeartmadeCandles.Tests/Order/BL/Services/OrderServiceTest.cs
+++ backend/HeartmadeCandles/HeartmadeCandles.Tests/Order/BL/Services/OrderServiceTest.cs
@@ -1,5 +1,137 @@
-﻿namespace HeartmadeCandles.UnitTests.Order.BL.Services;
+﻿using System.Text;
+using Bogus;
+using CSharpFunctionalExtensions;
+using HeartmadeCandles.Order.BL.Services;
+using HeartmadeCandles.Order.Core.Interfaces;
+using HeartmadeCandles.Order.Core.Models;
+using Moq;
+
+namespace HeartmadeCandles.UnitTests.Order.BL.Services;
 
 public class OrderServiceTest
 {
+    private static readonly Faker _faker = new();
+    private readonly Mock<IOrderNotificationHandler> _orderNotificationHandler = new(MockBehavior.Strict);
+
+    private readonly Mock<IOrderRepository> _orderRepository = new(MockBehavior.Strict);
+
+    private readonly OrderService _service;
+
+    public OrderServiceTest()
+    {
+        _service = new OrderService(_orderRepository.Object, _orderNotificationHandler.Object);
+    }
+
+    [Fact]
+    public async Task Get_WhenValid_ShouldReturnValidOrderItems()
+    {
+        // Arrange
+        var orderItemFilters = new List<OrderItemFilter>();
+        for (var i = 0; i < _faker.Random.Number(1, 100); i++)
+            orderItemFilters.Add(GenerateOrderData.GenerateOrderItemFilter());
+
+        var orderItems = new List<OrderItem>();
+        for (var i = 0; i < orderItemFilters.Count; i++)
+            orderItems.Add(GenerateOrderData.GenerateOrderItem(orderItemFilters[i]));
+
+        _orderRepository.Setup(or => or.Get(orderItemFilters.ToArray()))
+            .ReturnsAsync(Result.Success(orderItems.ToArray()))
+            .Verifiable();
+
+        // Act
+        var result = await _service.Get(orderItemFilters.ToArray());
+
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task Get_WhenOneOrderItemInvalid_ShouldReturnFailure()
+    {
+        // Arrange
+        var orderItemFilters = new List<OrderItemFilter>();
+        for (var i = 0; i < _faker.Random.Number(1, 100); i++)
+            orderItemFilters.Add(GenerateOrderData.GenerateOrderItemFilter());
+
+        var invalidDecor = GenerateOrderData.GenerateDecor(_faker.Random.Number(1, 10000));
+        var validOrderItem = GenerateOrderData.GenerateOrderItem(orderItemFilters[0]);
+        var invalidCandleDetail = new CandleDetail(
+            validOrderItem.CandleDetail.Candle, invalidDecor, validOrderItem.CandleDetail.LayerColors,
+            validOrderItem.CandleDetail.NumberOfLayer, validOrderItem.CandleDetail.Smell,
+            validOrderItem.CandleDetail.Wick);
+        var invalidOrderItem = OrderItem.Create(
+            invalidCandleDetail, validOrderItem.Quantity, validOrderItem.OrderItemFilter);
+
+        var orderItems = new List<OrderItem>();
+        orderItems.Add(invalidOrderItem.Value);
+
+        for (var i = 0; i < orderItemFilters.Count - 1; i++)
+            orderItems.Add(GenerateOrderData.GenerateOrderItem(orderItemFilters[i]));
+
+        _orderRepository.Setup(or => or.Get(orderItemFilters.ToArray()))
+            .ReturnsAsync(Result.Success(orderItems.ToArray()))
+            .Verifiable();
+
+        // Act
+        var result = await _service.Get(orderItemFilters.ToArray());
+
+        // Assert
+        Assert.True(result.IsFailure);
+        Assert.Equal(
+            result.Error,
+            $"Decor by id: {validOrderItem.CandleDetail.Decor?.Id} does not match with decor by id: {invalidOrderItem.Value.CandleDetail.Decor?.Id}");
+    }
+
+    [Fact]
+    public async Task Get_WhenAllOrderItemInvalid_ShouldReturnFailure()
+    {
+        // Arrange
+        var orderItemFilters = new List<OrderItemFilter>();
+        for (var i = 0; i < _faker.Random.Number(1, 100); i++)
+            orderItemFilters.Add(GenerateOrderData.GenerateOrderItemFilter());
+
+        var orderItems = new List<OrderItem>();
+        var validDecorIds = new List<int>();
+        var invalidDecorIds = new List<int>();
+
+        for (var i = 0; i < orderItemFilters.Count; i++)
+        {
+            var invalidDecor = GenerateOrderData.GenerateDecor(_faker.Random.Number(1, 10000));
+            var validOrderItem = GenerateOrderData.GenerateOrderItem(orderItemFilters[0]);
+            var invalidCandleDetail = new CandleDetail(
+                validOrderItem.CandleDetail.Candle, invalidDecor, validOrderItem.CandleDetail.LayerColors,
+                validOrderItem.CandleDetail.NumberOfLayer, validOrderItem.CandleDetail.Smell,
+                validOrderItem.CandleDetail.Wick);
+            var invalidOrderItem = OrderItem.Create(
+                invalidCandleDetail, validOrderItem.Quantity, validOrderItem.OrderItemFilter);
+
+            validDecorIds.Add(validOrderItem.CandleDetail.Decor?.Id ?? 0);
+            invalidDecorIds.Add(invalidOrderItem.Value.CandleDetail.Decor?.Id ?? 0);
+
+            orderItems.Add(invalidOrderItem.Value);
+        }
+
+        _orderRepository.Setup(or => or.Get(orderItemFilters.ToArray()))
+            .ReturnsAsync(Result.Success(orderItems.ToArray()))
+            .Verifiable();
+
+        var errorMessage = new StringBuilder();
+        for (var i = 0; i < validDecorIds.Count && i < invalidDecorIds.Count; i++)
+        {
+            if (i > 0)
+            {
+                errorMessage.Append(", ");
+            }
+
+            errorMessage.Append(
+                $"Decor by id: {validDecorIds[i]} does not match with decor by id: {invalidDecorIds[i]}");
+        }
+
+        // Act
+        var result = await _service.Get(orderItemFilters.ToArray());
+
+        // Assert
+        Assert.True(result.IsFailure);
+        Assert.Equal(errorMessage.ToString(), result.Error);
+    }
 }
--- backend/HeartmadeCandles/HeartmadeCandles.Tests/Order/GenerateOrderData.cs
+++ backend/HeartmadeCandles/HeartmadeCandles.Tests/Order/GenerateOrderData.cs
@@ -8,10 +8,10 @@ internal class GenerateOrderData
 {
     private static readonly Faker _faker = new();
 
-    public static Candle GenerateCandle()
+    public static Candle GenerateCandle(int id = 0)
     {
         return new Candle(
-            _faker.Random.Number(1, 10000),
+            id == 0 ? _faker.Random.Number(1, 10000) : id,
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Candle.MaxTitleLength),
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Candle.MaxDescriptionLength),
             _faker.Random.Number(1, 10000) * _faker.Random.Decimal(),
@@ -24,10 +24,10 @@ internal class GenerateOrderData
         );
     }
 
-    public static Decor GenerateDecor()
+    public static Decor GenerateDecor(int id = 0)
     {
         return new Decor(
-            _faker.Random.Number(1, 10000),
+            id == 0 ? _faker.Random.Number(1, 10000) : id,
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Decor.MaxTitleLength),
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Decor.MaxDescriptionLength),
             _faker.Random.Number(1, 10000) * _faker.Random.Decimal(),
@@ -39,11 +39,11 @@ internal class GenerateOrderData
         );
     }
 
-    public static LayerColor GenerateLayerColor()
+    public static LayerColor GenerateLayerColor(int id = 0)
     {
         return new LayerColor
         (
-            _faker.Random.Number(1, 10000),
+            id == 0 ? _faker.Random.Number(1, 10000) : id,
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.LayerColor.MaxTitleLength),
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.LayerColor.MaxDescriptionLength),
             _faker.Random.Number(1, 10000) * _faker.Random.Decimal(),
@@ -55,31 +55,31 @@ internal class GenerateOrderData
         );
     }
 
-    public static NumberOfLayer GenerateNumberOfLayer()
+    public static NumberOfLayer GenerateNumberOfLayer(int id = 0)
     {
+        var number = id == 0 ? _faker.Random.Number(1, 10000) : id;
         return new NumberOfLayer
         (
-            _faker.Random.Number(1, 10000),
-            _faker.Random.Number(1, 10000)
+            number, number
         );
     }
 
-    public static Smell GenerateSmell()
+    public static Smell GenerateSmell(int id = 0)
     {
         return new Smell
         (
-            _faker.Random.Number(1, 10000),
+            id == 0 ? _faker.Random.Number(1, 10000) : id,
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Smell.MaxTitleLength),
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Smell.MaxDescriptionLength),
             _faker.Random.Number(1, 10000) * _faker.Random.Decimal()
         );
     }
 
-    public static Wick GenerateWick()
+    public static Wick GenerateWick(int id = 0)
     {
         return new Wick
         (
-            _faker.Random.Number(1, 10000),
+            id == 0 ? _faker.Random.Number(1, 10000) : id,
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Wick.MaxTitleLength),
             _faker.Random.String(1, HeartmadeCandles.Admin.Core.Models.Wick.MaxDescriptionLength),
             _faker.Random.Number(1, 10000) * _faker.Random.Decimal(),
@@ -114,5 +114,38 @@ internal class GenerateOrderData
         var wickId = candleDetail.Wick.Id;
 
         return new OrderItemFilter(candleId, decorId, numberOfLayerId, layerColorIds, smellId, wickId, quantity);
+    }
+
+    public static OrderItemFilter GenerateOrderItemFilter()
+    {
+        var candleDetail = GenerateCandleDetail();
+        var quantity = _faker.Random.Number(1, 100);
+
+        var candleId = candleDetail.Candle.Id;
+        var decorId = candleDetail.Decor?.Id ?? 0;
+        var numberOfLayerId = candleDetail.NumberOfLayer.Id;
+        var layerColorIds = candleDetail.LayerColors.Select(l => l.Id).ToArray();
+        var smellId = candleDetail.Smell?.Id ?? 0;
+        var wickId = candleDetail.Wick.Id;
+
+        return new OrderItemFilter(candleId, decorId, numberOfLayerId, layerColorIds, smellId, wickId, quantity);
+    }
+
+    public static OrderItem GenerateOrderItem(OrderItemFilter orderItemFilter)
+    {
+        var candle = GenerateCandle(orderItemFilter.CandleId);
+        var decor = GenerateDecor(orderItemFilter.DecorId);
+        var layerColors = new List<LayerColor>();
+        for (var i = 0; i < orderItemFilter.LayerColorIds.Length; i++)
+            layerColors.Add(GenerateLayerColor(orderItemFilter.LayerColorIds[i]));
+        var numberOfLayer = GenerateNumberOfLayer(orderItemFilter.NumberOfLayerId);
+        var smell = GenerateSmell(orderItemFilter.SmellId);
+        var wick = GenerateWick(orderItemFilter.WickId);
+
+        var candleDetail = new CandleDetail(candle, decor, layerColors.ToArray(), numberOfLayer, smell, wick);
+
+        var orderItem = OrderItem.Create(candleDetail, orderItemFilter.Quantity, orderItemFilter);
+
+        return orderItem.Value;
     }
 }
